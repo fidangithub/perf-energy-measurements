b55f2d4523e04f7040474939e55e6ade02e62028
Fidan <fidan@pentester.com>
2025-04-15 10:40:53 +0400

    fix: cleanup mismatched tree definition

--- treebylevels.rs
+++ treebylevels.rs
@@ -2,12 +2,6 @@ mod preloaded;
 use preloaded::Node;
 use std::collections::VecDeque;
 
-struct Node {
-  value: u32,
-  left: Option<Box<Node>>,
-  right: Option<Box<Node>>
-}
-
 fn tree_by_levels(root: &Node) -> Vec<u32> {
     let mut result = vec![];
     let mut queue: VecDeque<&Node> = VecDeque::from([root]);
