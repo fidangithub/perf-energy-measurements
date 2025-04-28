1ee54de9c23f8af04a0ad55022b5bdfd3070c328
fidangithub <fidansuleymanova100@gmail.com>
2025-04-28 19:14:11 +0400

    fix: change the path for idle power watss

--- idle_power_watts.py
+++ idle_power_watts.py
@@ -1,9 +1,12 @@
 # Python example:
 import pandas as pd
 
-df = pd.read_csv("idle_energy_results.csv")
+df = pd.read_csv("./perf-energy-measurements/idle_energy_results.csv")
 avg_joules = df["energy_joules"].mean()
 avg_time = df["time_seconds"].mean()
 idle_power_watts = avg_joules / avg_time
 
 print(f"Average Idle Power: {idle_power_watts:.4f} Watts")
+
+
+# Average Idle Power: 0.6559 Watts
