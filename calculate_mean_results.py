2ca71accfc220fb65a102588bd4befafab3671a7
fidangithub <fidansuleymanova100@gmail.com>
2025-04-29 22:32:21 +0400

    feat: final rounded results

--- calculate_mean_results.py
+++ calculate_mean_results.py
@@ -6,8 +6,8 @@ energy_data = pd.read_csv(file_path)
 energy_cols = [f'e{i}' for i in range(1, 31)]
 time_cols = [f't{i}' for i in range(1, 31)]
 
-energy_data['mean_energy'] = energy_data[energy_cols].mean(axis=1).round(1)
-energy_data['mean_time'] = energy_data[time_cols].mean(axis=1).round(1)
+energy_data['mean_energy'] = energy_data[energy_cols].mean(axis=1).round(2)
+energy_data['mean_time'] = energy_data[time_cols].mean(axis=1).round(2)
 
 energy_data.to_csv('./perf-energy-measurements/final_energy_results_round.csv', index=False)
 
