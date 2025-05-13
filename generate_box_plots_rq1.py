d3f78b87daee10f2ccb5a8f4cb25431890e9045b
Fidan <fidan@pentester.com>
2025-05-13 13:02:22 +0400

    fix: box plots

--- generate_box_plots_rq1.py
+++ generate_box_plots_rq1.py
@@ -9,10 +9,10 @@ file_path = './perf-energy-measurements/final_energy_results_round.csv'
 df = pd.read_csv(file_path)
 
 # Preprocess
-df = df[~df['file'].str.contains('_opt')]  # Remove _opt
-df['source'] = df['file'].apply(lambda x: 'human' if '.' not in x else x.split('.')[-1])
-df['language'] = df['file'].apply(lambda x: x.split('_')[-1].split('.')[0] if '.' in x else x.split('_')[-1])
-df['task'] = df['file'].apply(lambda x: '_'.join(x.split('_')[:-1]) if '.' not in x else '_'.join(x.split('_')[:-1]).split('.')[0])
+df = df[~df['script'].str.contains('_opt')]  # Remove _opt
+df['source'] = df['script'].apply(lambda x: 'human' if '.' not in x else x.split('.')[-1])
+df['language'] = df['script'].apply(lambda x: x.split('_')[-1].split('.')[0] if '.' in x else x.split('_')[-1])
+df['task'] = df['script'].apply(lambda x: '_'.join(x.split('_')[:-1]) if '.' not in x else '_'.join(x.split('_')[:-1]).split('.')[0])
 
 # Normalize time and energy for better comparability on boxplots
 df['norm_time'] = df['time'] / df['time'].max()
